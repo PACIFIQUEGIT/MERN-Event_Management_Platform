028198098bc93a9ef2462ec6224b66e5e2eb432b
pacifiquegit <mutanguhapacifique@gmail.com>
2025-05-28 04:39:12 +0200

    dotenv config

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import EventCard from '../components/EventCard';
 
 const baseUrl = process.env.REACT_APP_API_BASE_URL || 'http://localhost:4000/api';
-
+console.log(process.env.REACT_APP_API_BASE_URL);
 export default function Home() {
   const [events, setEvents] = useState([]);
   const [loading, setLoading] = useState(true);
