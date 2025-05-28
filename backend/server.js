b019707f0bd12ba824206d409055e8078e9c2cc4
pacifiquegit <mutanguhapacifique@gmail.com>
2025-05-28 04:32:45 +0200

    dotenv config

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const app = require('./app');
+const { config } = require("dotenv")
+config()
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
